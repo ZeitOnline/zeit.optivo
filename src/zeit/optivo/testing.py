f793c71a66d1bdb5c44dceac9bbdcf00201b3bbd
Wolfgang Schnerring <ws@gocept.com>
2014-03-27 10:27:36 +0100

    Implement mock version of our API for testing

--- src/zeit/optivo/testing.py
+++ src/zeit/optivo/testing.py
@@ -1,5 +1,35 @@
+import logging
+import zeit.optivo.interfaces
+import zope.interface
+
+
+log = logging.getLogger(__name__)
+
+
 settings = {
     'username': 'stefan.ehlers@zeit.de',
     'password': 'zeitonline2014',
     'mandant': '74276897418',
 }
+
+
+class Optivo(object):
+
+    zope.interface.implements(zeit.optivo.interfaces.IOptivo)
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.calls = []
+
+    def send(self, mandant, subject, html, text):
+        self.calls.append(('send', mandant, subject, html, text))
+        log.info('Optivo.send(%s)', dict(
+            mandant=mandant, subject=subject, html=html, text=text))
+
+    def test(self, mandant, to, subject, html, text):
+        self.calls.append(('test', mandant, to, subject, html, text))
+        log.info('Optivo.test(%s)', dict(
+            mandant=mandant, to=to,
+            subject=subject, html=html, text=text))
