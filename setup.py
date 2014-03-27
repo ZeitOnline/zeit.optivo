c716a1964a0b88e729e5957221e4ad8b337ccc6f
Wolfgang Schnerring <ws@gocept.com>
2014-03-27 08:43:22 +0100

    Create skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.optivo',
+    version='1.0.0.dev0',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='https://bitbucket.org/gocept/zeit.optivo',
+    description="Optivo SOAP-API client for sending emails",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages=['zeit'],
+    install_requires=[
+        'grokcore.component',
+        'setuptools',
+        'suds',
+        'zope.component',
+        'zope.interface',
+    ],
+)
